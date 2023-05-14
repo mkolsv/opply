50bd761f11e310f509b53a45e91ae8952f2333c5
Misha Kolosov <mykhailo.kolosov@masterofcode.com>
2023-05-14 11:52:18 +0200

    add home page

---
+++ src/components/home/HomePage.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="home-page__wrapper container">
+        <SuppliersList />
+    </div>
+</template>
+
+<script setup lang="ts">
+import { onBeforeMount } from 'vue';
+import { useOpplyStore } from '../../store';
+import SuppliersList from './SuppliersList.vue';
+
+const store = useOpplyStore();
+
+onBeforeMount(async () => {
+    await store.fetchSuppliers(1);
+});
+</script>
+
+<style scoped>
+.home-page__wrapper {
+    display: flex;
+    justify-content: center;
+    flex-wrap: wrap;
+    gap: 24px;
+    padding-top: 24px;
+}
+</style>
