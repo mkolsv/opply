5877f0fcac38577db8d76b453efa3007f1aa90fd
Misha Kolosov <mykhailo.kolosov@masterofcode.com>
2023-05-14 11:53:39 +0200

    add base tab component

---
+++ src/components/shared/tabs/Tab.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="tabs__content" v-if="selectedTitle === title">
+        <slot />
+    </div>
+</template>
+
+<script setup lang="ts">
+import { inject } from 'vue';
+
+const props = defineProps({
+    title: {
+        type: String,
+        required: true
+    }
+})
+
+const selectedTitle = inject('selectedTitle');
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/shared/tabs/Tabs.vue
@@ -0,0 +1,55 @@
+<template>
+    <div class="tabs__wrapper">
+        <ul class="tabs">
+            <li
+                class="tabs__item"
+                v-for="title in tabTitles"
+                :key="title"
+                @click="selectedTitle = title"
+            >
+                <button
+                    class="tabs__item-button"
+                    :class="{ 'tabs__item-button--selected': selectedTitle === title}"
+                >
+                    {{ title }}
+                </button>
+            </li>
+        </ul>
+
+        <slot />
+    </div>
+</template>
+
+<script setup lang="ts">
+import { useSlots, ref , provide } from 'vue';
+
+defineProps({
+    tabs: {
+        type: Array,
+        required: true
+    }
+});
+
+const slots = useSlots();
+const tabTitles = ref(slots.default().map(tab => tab.props.title));
+const selectedTitle = ref(tabTitles.value[0]);
+
+provide('selectedTitle', selectedTitle);
+</script>
+
+<style scoped>
+.tabs {
+    display: flex;
+    justify-content: center;
+    gap: 12px;
+}
+
+.tabs__item {
+
+}
+
+.tabs__item-button--selected {
+    background-color: var(--background-color-secondary);
+    color: var(--text-color-secondary);
+}
+</style>
