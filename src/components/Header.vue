3bdc40bbe5aabc70c311322941f60860bf916c20
Misha Kolosov <mykhailo.kolosov@masterofcode.com>
2023-05-14 11:52:07 +0200

    add basic header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,6 @@
 <template>
-    <header>
-        <h1>Opply</h1>
+    <header class="header container">
+        <h1 class="header__title">Opply</h1>
     </header>
 </template>
 
@@ -9,5 +9,13 @@
 </script>
 
 <style scoped>
+.header {
+    background-color: var(--background-color-secondary);
+    padding: 24px;
+}
 
+.header__title {
+    font-size: 16px;
+    color: var(--text-color-secondary);
+}
 </style>
