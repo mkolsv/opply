54137bbfa842f1e4179f4b41b9e3e67521b9906a
Misha Kolosov <mykhailo.kolosov@masterofcode.com>
2023-05-14 11:51:59 +0200

    create auth page

--- src/components/auth/AuthPage.vue
+++ src/components/auth/AuthPage.vue
@@ -1,11 +1,19 @@
 <template>
-
+    <section>
+        <Tabs>
+            <Tab title="Sign In">
+                <SignIn />
+            </Tab>
+            <Tab title="Registration">
+                <Registration />
+            </Tab>
+        </Tabs>
+    </section>
 </template>
 
 <script setup lang="ts">
-
+import Tabs from '../shared/tabs/Tabs.vue';
+import Tab from '../shared/tabs/Tab.vue';
+import SignIn from './signIn/SignIn.vue';
+import Registration from './registration/Registration.vue';
 </script>
-
-<style scoped>
-
-</style>
