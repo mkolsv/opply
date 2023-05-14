b746dc0b5e9d7cd2400af45aed3cb1408710c205
Misha Kolosov <mykhailo.kolosov@masterofcode.com>
2023-05-14 11:51:51 +0200

    create home page

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,23 @@
 <template>
-
+    <Header />
+    <AuthPage v-if="!isAuthorized" />
+    <HomePage v-else />
 </template>
 
 <script setup lang="ts">
-import { useOpplyStore } from "./store";
+import { computed } from 'vue';
+import { useOpplyStore } from './store';
+import Header from './components/Header.vue';
+import AuthPage from './components/auth/AuthPage.vue';
+import HomePage from "./components/home/HomePage.vue";
 
 const store = useOpplyStore();
+
+const isAuthorized = computed(() => {
+    return !!localStorage.getItem('token');
+});
+
+console.log(isAuthorized);
 </script>
 
 <style scoped>
