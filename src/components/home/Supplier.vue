59563a9d2c2fa2a5ea89b472ca7f061904265b29
Misha Kolosov <mykhailo.kolosov@masterofcode.com>
2023-05-14 11:53:10 +0200

    suppliers layout

---
+++ src/components/home/Supplier.vue
@@ -0,0 +1,34 @@
+<template>
+<div class="supplier__card">
+    <a class="supplier__name">{{ supplier.name }}</a>
+    <p class="supplier__description">{{ supplier.description }}</p>
+</div>
+</template>
+
+<script setup lang="ts">
+import { Supplier } from '../../types';
+
+defineProps<{ supplier: Supplier }>();
+</script>
+
+<style scoped>
+.supplier__card {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    border-radius: 2px;
+    width: 300px;
+    padding: 12px;
+    background-color: var(--background-color-accent);
+}
+
+.supplier__name {
+    color: var(--link-color-secondary);
+    text-transform: uppercase;
+}
+
+.supplier__description {
+    color: var(--text-color-secondary);
+    font-size: 14px;
+}
+</style>
---
+++ src/components/home/SuppliersList.vue
@@ -0,0 +1,16 @@
+<template>
+    <Supplier v-for="supplier in suppliers" :key="supplier.id" :supplier="supplier" />
+</template>
+
+<script setup lang="ts">
+import { useOpplyStore } from '../../store';
+import Supplier from './Supplier.vue';
+
+const store = useOpplyStore();
+
+const suppliers = store.suppliers;
+</script>
+
+<style scoped>
+
+</style>
