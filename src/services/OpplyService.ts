bd0fa9a9d57dce4db56e05d8528795a17462fcd9
Misha Kolosov <mykhailo.kolosov@masterofcode.com>
2023-05-14 11:53:30 +0200

    add service

---
+++ src/services/OpplyService.ts
@@ -0,0 +1,64 @@
+import { AxiosStatic } from 'axios';
+
+export interface OpplyServices {
+    httpClient: AxiosStatic
+    apiUrl: string
+}
+export class OpplyService {
+    _httpClient: AxiosStatic;
+    _apiUrl: string
+
+    static create({ httpClient, apiUrl }: OpplyServices) {
+        console.log(import.meta.env)
+        return new OpplyService({
+            httpClient,
+            apiUrl
+        });
+    }
+
+    constructor({ httpClient, apiUrl }: OpplyServices) {
+        this._apiUrl = apiUrl;
+        this._httpClient = httpClient;
+        this._addDefaultHeaders();
+    }
+
+    async signUp(userData: Object) {
+        try {
+            const response = await this._httpClient.post(`${this._apiUrl}/api/v1/users/`, userData);
+
+            console.log('response', response.data);
+        } catch (error: any) {
+            throw error.response.data;
+        }
+    }
+
+    async signIn(userData: object) {
+        try {
+            const response = await this._httpClient.post(`${this._apiUrl}/api-token-auth/`, userData);
+
+            this._saveToken(response.data.token);
+        } catch (error: any) {
+            throw error.response.data;
+        }
+    }
+
+    async fetchSuppliers(page: number) {
+        try {
+            const response = await this._httpClient.get(`${this._apiUrl}/api/v1/suppliers/`, { params: { page } });
+
+            return response.data;
+        } catch (error: any) {
+            throw error.response.data;
+        }
+    }
+
+    _addDefaultHeaders() {
+        if (!localStorage.getItem('token')) return;
+
+        this._httpClient.defaults.headers['Authorization'] = `Token ${localStorage.getItem('token')}`;
+    }
+
+    _saveToken(token: string) {
+        localStorage.setItem('token', token);
+    }
+}
