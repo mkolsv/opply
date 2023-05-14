af2a264aa34261dd5cda85212b0b5d444aab292d
Misha Kolosov <mykhailo.kolosov@masterofcode.com>
2023-05-14 11:52:38 +0200

    create store

--- src/store/opplyStore.ts
+++ src/store/opplyStore.ts
@@ -1,7 +1,27 @@
 import { defineStore } from 'pinia';
+import { opplyService } from '../services';
+import { Supplier } from '../types';
 
 export const useOpplyStore = defineStore('opply', {
     state: () => ({
-        isAuthorized: false
-    })
+        suppliers: [] as Supplier[]
+    }),
+
+    actions: {
+        async signUp(userData: Object) {
+            await opplyService.signUp(userData);
+        },
+
+        async signIn(userData: Object) {
+            await opplyService.signIn(userData);
+        },
+
+        async fetchSuppliers(page: number) {
+            const response = await opplyService.fetchSuppliers(page);
+
+            console.log(response.results)
+
+            this.suppliers.push(...response.results);
+        }
+    }
 });
